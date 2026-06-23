10d1926cd6da2f6099cbac05909e4bcc6d1310c5
daniellima0337 <daniel.lima0337@gmail.com>
2026-06-23 20:58:31 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Código base JavaScript
+
+// Função principal
+function main() {
+  console.log("Bem-vindo ao projeto!");
+}
+
+// Event listeners
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
+
+// Funções utilitárias
+function exemplo() {
+  // Adicione seu código aqui
+}
+
+
